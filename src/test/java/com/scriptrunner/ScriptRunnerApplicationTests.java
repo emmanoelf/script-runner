aa881be72b90fee1824bc86d136fde55d1e0af61
emmanoel <emmanoel_95@hotmail.com>
2026-03-11 15:44:37 -0300

    build: base project

---
+++ src/test/java/com/scriptrunner/ScriptRunnerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scriptrunner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScriptRunnerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
